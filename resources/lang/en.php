79d5753a32724543cfd9169c61a97f2e9c8d705b
root <root@srv868851.hstgr.cloud>
2025-06-26 17:31:05 +0000

    Laravel12

---
+++ resources/lang/en.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'Intelboard' => 'Intelboard',
+    'Sign in Intelboard' => 'Sign in Intelboard',
+    'Username Or Email Address' => 'Username Or Email Address',
+    'Email address' => 'Email address',
+    'Password' => 'Password',
+    'Keep me logged in' => 'Keep me logged in',
+    'Forget password?' => 'Forget password?',
+    'sign in' => 'Sign in',
+    'Or' => 'Or',
+    "Don't have an account?" => "Don't have an account?",
+    'Sign up' => 'Sign up',
+];
---
+++ resources/lang/fr.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'Intelboard' => 'Intelboard',
+    'Sign in Intelboard' => 'Connexion à Intelboard',
+    'Username Or Email Address' => "Nom d'utilisateur ou adresse e-mail",
+    'Email address' => 'Adresse e-mail',
+    'Password' => 'Mot de passe',
+    'Keep me logged in' => 'Rester connecté',
+    'Forget password?' => 'Mot de passe oublié ?',
+    'sign in' => 'Se connecter',
+    'Or' => 'Ou',
+    "Don't have an account?" => "Vous n'avez pas de compte ?",
+    'Sign up' => "S'inscrire",
+];
